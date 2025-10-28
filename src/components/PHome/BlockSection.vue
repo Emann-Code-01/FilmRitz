570c63955b34ea750b74d1adb4cc8ff855f81faa
Emann-script <olajubajeifeoluwa93@gmail.com>
2025-10-28 11:26:31 +0100

    changed "Get started" to "Sign In"

--- src/components/PHome/BlockSection.vue
+++ src/components/PHome/BlockSection.vue
@@ -840,7 +840,7 @@
           class="gap-3 bg-[#b20710] text-white focus:outline-none font-[Gilroy-Bold] md:text-2xl px-8 py-4 md:py-3 rounded-sm hover:bg-[#e32125] group transition-all duration-500"
           to="/ng/login"
         >
-          Get Started
+          Sign In
           <i class="pi pi-chevron-right text-xl group-hover:animate-pulse"></i>
         </router-link>
       </div>
